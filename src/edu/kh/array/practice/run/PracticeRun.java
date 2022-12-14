05bdb455119035c5ef4e509f9035fdc202b89c17
LJAEH <wogurdlek33@gmail.com>
2022-12-14 15:13:48 +0900

    Revert "array2debug"
    This reverts commit d6b7f3ed0d88837be633bd13b4c49420c0d274d6.

--- src/edu/kh/array/practice/run/PracticeRun.java
+++ src/edu/kh/array/practice/run/PracticeRun.java
@@ -8,7 +8,7 @@ public class PracticeRun {
 		
 		PracticeService arrex = new PracticeService();
 		
-		arrex.practice1();
+		arrex.practice13();
 		
 		
 	}
--- src/edu/kh/array/practice/service/PracticeService.java
+++ src/edu/kh/array/practice/service/PracticeService.java
@@ -12,25 +12,19 @@ public class PracticeService {
 
 	
 	public void practice1() {
-		/*
-		 * 길이가 9인 배열을 선언 및 할당하고, 1부터 9까지의 값을 반복문을 이용하여
+		/*길이가 9인 배열을 선언 및 할당하고, 1부터 9까지의 값을 반복문을 이용하여
 		순서대로 배열의 각 인덱스 요소에 대입하고 출력한 후
 		짝수 번째 인덱스 값의 합을 출력하세요. (0 번째 인덱스는 짝수로 취급)
-
-		[실행 화면]
-		1 2 3 4 5 6 7 8 9
-		짝수 번째 인덱스 합 : 25
 		*/
 		
 		int[] arr = new int[9];
-		
 		int sum = 0;
 		
 		for( int i = 0; i < arr.length; i++) {
 			
 			arr[i] = (i+1);
 			if (i%2 == 0) {
-				sum += (arr[i]);
+				sum += (i);
 			}
 			
 			// 01 23 45 67 89 
@@ -38,21 +32,15 @@ public class PracticeService {
 		}
 		
 		System.out.println(Arrays.toString(arr));
-		System.out.println("짝수 번째 인덱스 합: " + sum);
+		System.out.println("짝수 번째 인덱스 합 : " + sum);
 		
 	}
 	
 	public void practice2() {
-		/*
-		 * 길이가 9인 배열을 선언 및 할당하고, 9부터 1까지의 값을 반복문을 이용하여
+		/*길이가 9인 배열을 선언 및 할당하고, 9부터 1까지의 값을 반복문을 이용하여
 		순서대로 배열의 각 인덱스 요소에 대입하고 출력한 후
 		홀수 번째 인덱스 값의 합을 출력하세요. (0 번째 인덱스는 짝수로 취급)
-
-		[실행 화면]
-		9 8 7 6 5 4 3 2 1
-		홀수 번째 인덱스 합 : 20
-		 */
-		
+		   */
 		int[] arr = new int[9];
 		int sum = 0;
 		
@@ -60,14 +48,14 @@ public class PracticeService {
 			
 			arr[i] = (arr.length-i);
 			if (i%2 != 0) {
-				sum += (arr[i]);
+				sum += (arr.length-i);
 			}
 			
 			// 18 36 54 72			
 		}
 		
 		System.out.println(Arrays.toString(arr));
-		System.out.println("홀수 번째 인덱스 합: " + sum);
+		System.out.println("홀수 번째 인덱스의 합 : " + sum);
 		
 		
 	}
@@ -75,26 +63,25 @@ public class PracticeService {
 	public void practice3() {
 		
 		/*사용자에게 입력 받은 양의 정수만큼 배열 크기를 할당하고
-		1부터 입력 받은 값까지 배열에 초기화한 후 출력하세요.
+		  1부터 입력 받은 값까지 배열에 초기화한 후 출력하세요.
 	     */
 		
-		System.out.print("정수 : ");
+		System.out.print("양의 정수 입력: ");
 		int input = sc.nextInt();
 		
 		int arr[] = new int[input];
 		
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = (i+1);
-
-			System.out.print(arr[i] + " ");
-			
 		}
+		
+		System.out.println(Arrays.toString(arr));
 		
 	}
 	
 	public void practice4() {
 		/* 
-		 정수 5개를 입력 받아 배열을 초기화 하고
+		 * 정수 5개를 입력 받아 배열을 초기화 하고
 		검색할 정수를 하나 입력 받아 배열에서 같은 수가 있는 인덱스를 찾아 출력.
 		배열에 같은 수가 없을 경우 “일치하는 값이 존재하지 않습니다“ 출력
 		*/
@@ -103,13 +90,13 @@ public class PracticeService {
 		
 		
 		for( int i = 0; i < arr.length; i++ ) {
-			System.out.print("����" + i + " : ");
+			System.out.print("입력" + i + " : ");
 			int x = sc.nextInt();
 			
 			arr[i] = x;
 		}
 		
-		System.out.print("寃����� 媛� : ");
+		System.out.print("검색할 값 : ");
 		int y = sc.nextInt();
 		int count = 0;
 		
@@ -121,19 +108,13 @@ public class PracticeService {
 		}
 		
 		if ( count == 0 ) {
-			System.out.println("�쇱����� 媛��� 議댁�ы��吏� ���듬����.");
+			System.out.println("일치하는 값이 존재하지 않습니다.");
 		}
 	}
 	
 	public void practice5() {
 		/* 문자열을 입력 받아 문자 하나하나를 배열에 넣고 검색할 문자가 문자열에 몇 개 들어가 있는지
 		개수와 몇 번째 인덱스에 위치하는지 인덱스를 출력하세요.
-
-		[실행 화면]
-		문자열 : application
-		문자 : i
-		application에 i가 존재하는 위치(인덱스) : 4 8
-		i 개수 : 2
 		*/
 		
 		System.out.print("문자열 : ");
@@ -145,11 +126,11 @@ public class PracticeService {
 			arr[i]=str.charAt(i);
 		}
 		
-		System.out.print("문자 :  ");
+		System.out.print("확인 할 문자 : ");
 		char alp = sc.next().charAt(0);
 		int count = 0;
 		
-		System.out.print(str + "에 " + alp + "가 존재하는 위치(인덱스) : ");
+		System.out.print(str+"에 i가 존재하는 위치(인덱스) : ");
 		for( int i = 0; i < arr.length; i++) {
 			if( arr[i] == alp ) {
 				System.out.print(i + " ");
@@ -166,17 +147,7 @@ public class PracticeService {
 		/* 사용자가 배열의 길이를 직접 입력하여 그 값만큼 정수형 배열을 선언 및 할당하고
 		배열의 크기만큼 사용자가 직접 값을 입력하여 각각의 인덱스에 값을 초기화 하세요.
 		그리고 배열 전체 값을 나열하고 각 인덱스에 저장된 값들의 합을 출력하세요.
-
-		[실행 화면]
-		정수 : 5
-		배열 0번째 인덱스에 넣을 값 : 4
-		배열 1번째 인덱스에 넣을 값 : -4
-		배열 2번째 인덱스에 넣을 값 : 3
-		배열 3번째 인덱스에 넣을 값 : -3
-		배열 4번째 인덱스에 넣을 값 : 2
-		4 -4 3 -3 2
-		총 합 : 2
-		 */
+		*/
 		
 		System.out.print("정수 : ");
 		int input = sc.nextInt();
@@ -191,22 +162,22 @@ public class PracticeService {
 			
 			arr[i] = a;
 			
-			sum += arr[i];
+			sum += a;
 		}
 		
 		System.out.println(Arrays.toString(arr));
 		
-		System.out.println("총 합 :  " + sum);
+		System.out.println("총합 : " + sum);
 		
 	}
 	
 	public void practice7() {
 
-		// 주민등록번호 번호를 입력 받아 성별을 나타내는 
-		// 숫자 이후부터 *로 가리고 출력하세요.
+		// 주민등록번호 번호를 입력 받아 성별을 나타내는 숫자 
+		// 이후부터 *로 가리고 출력하세요.
 		
 		
-		System.out.print("二쇰�쇰�깅�踰���(-�ы��) : ");
+		System.out.print("주민등록번호(-포함) : ");
 		
 		String str = sc.nextLine();
 		
@@ -225,13 +196,13 @@ public class PracticeService {
 	
 	public void practice8() {
 		/* 3이상인 홀수를 입력 받아 배열의 중간까지는 1부터 1씩 증가하여 오름차순으로 값을 넣고,
-중간 이후부터 끝까지는 1씩 감소하여 내림차순으로 값을 넣어 출력하세요.
-단, 입력한 정수가 홀수가 아니거나 3 미만일 경우 “다시 입력하세요”를 출력하고
-다시 정수를 받도록 하세요.
+		중간 이후부터 끝까지는 1씩 감소하여 내림차순으로 값을 넣어 출력하세요.
+		단, 입력한 정수가 홀수가 아니거나 3 미만일 경우 “다시 입력하세요”를 출력하고
+		다시 정수를 받도록 하세요.
 		*/
 		
 		for ( int re = 1; re >= 1; re++) {
-		System.out.print("���� : ");
+		System.out.print("홀수 : ");
 		int a = sc.nextInt();
 		
 		int[] arr = new int[a];
@@ -252,7 +223,7 @@ public class PracticeService {
 			break;
 			
 		} else {
-			System.out.println("�ㅼ�� ���ν���몄��.");
+			System.out.println("다시 입력하세요.");
 		}
 		
 		
@@ -263,10 +234,7 @@ public class PracticeService {
 	public void practice9() {
 		
 		/*10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고,
-1~10 사이의 난수를 발생시켜 배열에 초기화한 후 출력하세요.
-
-[실행 화면]
-발생한 난수 : 9 7 6 2 5 10 7 2 9 6
+		1~10 사이의 난수를 발생시켜 배열에 초기화한 후 출력하세요.
 		*/
 		
 		int[] ten = new int[10];
@@ -279,7 +247,7 @@ public class PracticeService {
 			
 		}
 		
-		System.out.println("諛����� ����: " + Arrays.toString(ten));
+		System.out.println("발생한 난수: " + Arrays.toString(ten));
 		
 		
 		
@@ -287,10 +255,13 @@ public class PracticeService {
 	
 	public void practice10() {
 		/*10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고,
-1~10 사이의 난수를 발생시켜 배열에 초기화한 후 출력하세요.
+		1~10 사이의 난수를 발생시켜 배열에 초기화 후
+		배열 전체 값과 그 값 중에서 최대값과 최소값을 출력하세요.
 
-[실행 화면]
-발생한 난수 : 9 7 6 2 5 10 7 2 9 6
+		[실행 화면]
+		발생한 난수 : 5 3 2 7 4 8 6 10 9 10
+		최대값 : 10
+		최소값 : 2
 		*/
 		
 		int[] ten = new int[10];
@@ -303,7 +274,7 @@ public class PracticeService {
 			
 		}
 		
-		System.out.println("諛����� ����: " + Arrays.toString(ten));
+		System.out.println("발생한 난수: " + Arrays.toString(ten));
 		
 		int max = ten[0];
 		
@@ -318,18 +289,15 @@ public class PracticeService {
 			}
 		}
 		
-		System.out.println("理���媛� : " + max);
-		System.out.println("理���媛� : " + min);
+		System.out.println("최대값 : " + max);
+		System.out.println("최소값 : " + min);
 		
 		
 	}
 	
 	public void practice11() {
 		/* 10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고
-1~10 사이의 난수를 발생시켜 중복된 값이 없게 배열에 초기화한 후 출력하세요.
-
-[실행 화면]
-4 1 3 6 9 5 8 10 7 2
+		1~10 사이의 난수를 발생시켜 중복된 값이 없게 배열에 초기화한 후 출력하세요.
 		*/
 		
 		int[] ten = new int[10];
@@ -351,16 +319,13 @@ public class PracticeService {
 			
 		}
 		
-		System.out.println("諛����� ����: " + Arrays.toString(ten));
+		System.out.println("발생한 난수: " + Arrays.toString(ten));
 	}
 	
 	public void practice12() {
 		
 		/*로또 번호 자동 생성기 프로그램을 만들기.
-(중복 값 없이 오름차순으로 정렬하여 출력하세요.)
-
-[실행 화면]
-3 4 15 17 28 40
+		(중복 값 없이 오름차순으로 정렬하여 출력하세요.)
 		*/
 		
 		int[] lotto = new int[6];
@@ -391,15 +356,10 @@ public class PracticeService {
 	public void practice13() {
 		
 		/* 문자열을 입력 받아 문자열에 어떤 문자가 들어갔는지 배열에 저장하고
-문자의 개수와 함께 출력하세요.
-
-[실행 화면]
-문자열 : application
-문자열에 있는 문자 : a, p, l, i, c, t, o, n
-문자 개수 : 8
+		문자의 개수와 함께 출력하세요.
 		*/
 		
-		System.out.print("臾몄��瑜� ���ν���몄�� : ");
+		System.out.print("문자를 입력하세여 : ");
 		
 		String str = sc.nextLine();
 		
@@ -423,9 +383,9 @@ public class PracticeService {
 		
 		System.out.println(length);
 		
-		/*System.out.println("臾몄���댁�� ���� 臾몄�� : " + Arrays.toString(arr));
+		/*System.out.println("문자열에 있는 문자 : " + Arrays.toString(arr));
 		
-		System.out.println("臾몄�� 媛��� : " + arr.length);
+		System.out.println("문자 개수 : " + arr.length);
 		
 		*/
 	}
